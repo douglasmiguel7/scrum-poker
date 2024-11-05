ed266ee95c65d463d81f2dcbd8cbf1bb0f485dd2
Douglas Andrade <douglasandrademiguel@gmail.com>
2024-11-05 02:36:18 -0300

    chore: add the basic firestore integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { doc, docData, Firestore } from '@angular/fire/firestore';
+import { traceUntilFirst } from '@angular/fire/performance';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scrum-poker';
+
+  public readonly testDocValue$: Observable<{ quantidade: number }>;
+
+  constructor(firestore: Firestore) {
+    const ref = doc(firestore, 'teste/config');
+    this.testDocValue$ = docData(ref).pipe(traceUntilFirst('firestore'));
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'scrum-poker-f387c',
+        appId: '1:257699867582:web:d4e569c7ad67e3a8a627ef',
+        storageBucket: 'scrum-poker-f387c.firebasestorage.app',
+        apiKey: 'AIzaSyCmUQk2t8bBqMx4-Ysv9LAU3iTRsMvUD2c',
+        authDomain: 'scrum-poker-f387c.firebaseapp.com',
+        messagingSenderId: '257699867582',
+        measurementId: 'G-NNZZNKMWBV',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
