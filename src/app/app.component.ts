fab41bccad22af464e74cc6f378180528f4ee7c9
Douglas Andrade <douglasandrademiguel@gmail.com>
2024-11-05 07:17:24 -0300

    chore: configure spartanui, check firestore connection, configure tailwind css, add not found page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { traceUntilFirst } from '@angular/fire/performance';
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, AsyncPipe],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { TablesComponent } from './tables/tables.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tables',
+    component: TablesComponent,
+  },
+  {
+    path: 'table',
+    component: TableComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
