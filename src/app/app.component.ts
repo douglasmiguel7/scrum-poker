1c1044d74e407a2cd2cb53412c5ae109aed1c56c
Douglas Andrade <douglasandrademiguel@gmail.com>
2024-11-06 03:31:59 -0300

    chore: add basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,27 @@ import { Component, OnInit } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { traceUntilFirst } from '@angular/fire/performance';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { Observable } from 'rxjs';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NzPageHeaderModule,
+    NzDropDownModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzTypographyModule,
+    NzFlexModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
